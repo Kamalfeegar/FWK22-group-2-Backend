0c60eef2958a24a321ae199fd96e8bae74942d1a
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-09-06 22:49:44 +0200

    Jest testing

--- __tests__/unit_tests.js
+++ __tests__/unit_tests.js
@@ -1,3 +1,13 @@
 /**
 @group unit
 **/
+
+//test with jest for domain/blog.js
+
+describe("Given:", () => {
+	describe("when:", () => {
+        test("Then:", () => {
+            expect().toBe();
+        });
+    });
+});
