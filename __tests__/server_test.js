cdc30a5942bff8c699412e50281ed6acf6b1f4cc
Viktor Ceder <viktor.alex.ceder@gmail.com>
2023-09-10 15:27:00 +0200

    (#33) Test not getting addres

--- __tests__/server_test.js
+++ __tests__/server_test.js
@@ -1,19 +1,10 @@
 const request = require('supertest');
 const { app } = require('../server');
 
-/* describe('GET /', () => {
-    it('responds with status code 200', async () => {
-      const response = await request(app).get('/blogpost');
-      expect(response.status).toBe(200);
-    });
-  }); */
 
   describe('GET /data', () => {
-    afterAll((done) => {
-      server.close(done);
-    });
     it('responds with a 200 status code', async () => {
       const response = await request(app).get('/blogpost');
       expect(response.status).toBe(200);
     });
-  });
+});
