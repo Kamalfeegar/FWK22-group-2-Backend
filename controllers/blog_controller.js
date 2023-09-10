81343a5119e35b0790549523d721c313c51ecff8
Viktor Ceder <viktor.alex.ceder@gmail.com>
2023-09-10 14:21:18 +0200

    (#33) controller created and all endpoints exist

---
+++ controllers/blog_controller.js
@@ -0,0 +1,39 @@
+
+const getAllArticles = async (req,res) =>{
+    try {
+        res.json()
+    } catch (error) {
+        console.log({ message: error });
+    }
+}
+const postArticle = async (req,res) =>{
+    try {
+        console.log({ message: error });
+    } catch (error) {
+        console.log({ message: error });
+    }
+}
+
+const getArticle = async (req,res) =>{
+    try {
+        console.log({ message: error });
+    } catch (error) {
+        console.log({ message: error });
+    }
+}
+
+const deleteArticle = async (req,res) =>{
+    try {
+        console.log({ message: error });
+    } catch (error) {
+        console.log({ message: error });
+    }
+}
+
+const uppdateArticle = async (req,res) =>{
+    try {
+        console.log({ message: error });
+    } catch (error) {
+        console.log({ message: error });
+    }
+}
