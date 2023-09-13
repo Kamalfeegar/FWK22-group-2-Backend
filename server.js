fd80dfb9d5931514d025e82c18a38ec27570b305
vikman472 <Viktor.alex.ceder@gmail.com>
2023-09-13 09:38:11 +0200

    (#33) remove db conektion

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 const express = require("express");
-const mongoose = require("mongoose")
+/* const mongoose = require("mongoose") */
 
 const app = express();
 
 //-.env processes
-const port = process.env.PORT
+const port = process.env.PORT;
 
 //-midle weare
 app.use(express.json());
 app.use(cors());
 
-//-Routing 
-const blogRoutes = require('./routes/blog_route');
+//-Routing
+const blogRoutes = require("./routes/blog_route");
 app.use("/blogpost", blogRoutes);
 
 /// Conect Mogodb to server
-mongoose.set("strictQuery", false);
+/* mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
@@ -25,10 +25,10 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected to MongoDB successfully");
 });
-
+ */
 // Link to a port
 app.listen(port, (err) => {
   console.log(`Listening on port: http://localhost:${port}/`);
 });
 
-module.exports = app
+module.exports = app;
