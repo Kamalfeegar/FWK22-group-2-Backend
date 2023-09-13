06cd8892c660545c81178322694f1d212252c3c0
Viktor <viktor.alex.ceder@gmail.com>
2023-09-13 21:50:15 +0200

    (#26)server now devided in service and server files

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-require("dotenv").config();
-const cors = require("cors");
-const express = require("express");
-/* const mongoose = require("mongoose") */
-
+const express = require('express');
 const app = express();
-
-//-.env processes
-const port = process.env.PORT;
-
-//-midle weare
 app.use(express.json());
-app.use(cors());
 
-//-Routing
 const blogRoutes = require("./routes/blog_route");
 app.use("/blogpost", blogRoutes);
 
+
+module.exports = app;
+
+
+
+
+
 /// Conect Mogodb to server
+
+/* const mongoose = require("mongoose") */
 /* mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DB_URL);
 
@@ -26,9 +24,3 @@ db.once("open", function () {
   console.log("Connected to MongoDB successfully");
 });
  */
-// Link to a port
-app.listen(port, (err) => {
-  console.log(`Listening on port: http://localhost:${port}/`);
-});
-
-module.exports = app;
