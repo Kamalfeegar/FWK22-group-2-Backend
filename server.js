58635183fbb17a1caa0f1c8f3094b216f0f91b61
vikman472 <Viktor.alex.ceder@gmail.com>
2023-09-18 11:10:23 +0200

    (#50)cors added

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
-
+app.use(cors());
 const blogRoutes = require("./routes/blog_route");
 app.use("/blogpost", blogRoutes);
 
-
 module.exports = app;
-
-
-
-
 
 /// Conect Mogodb to server
 
