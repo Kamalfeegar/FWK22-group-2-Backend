d6fbdf7db46c30ae2d214763c34210328679cb49
Viktor <viktor.alex.ceder@gmail.com>
2023-09-25 21:07:01 +0200

    (#67)husky first test

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 app.use(express.json());
 app.use(cors());
+
 const blogRoutes = require("./routes/blog_route");
 app.use("/blogpost", blogRoutes);
 
