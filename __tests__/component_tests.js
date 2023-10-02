c4a8aa7d28ff2fc0bd1b6d943e3e8daa8aacf5a6
Viktor <viktor.alex.ceder@gmail.com>
2023-10-02 11:31:58 +0200

    (#77) test yamel pipeline @Robinrac

--- __tests__/component_tests.js
+++ __tests__/component_tests.js
@@ -1,3 +1,6 @@
-/**
- *  @group component
- */
+// Test if Jest is working
+describe('Test jest', () => {
+    it('1 = 1', () => {
+      expect(1).toBe(1);
+    });
+});
--- __tests__/integration_tests.js
+++ __tests__/integration_tests.js
@@ -1,3 +1,6 @@
-/**
- * @group integration
- */
+// Test if Jest is working
+describe('Test jest', () => {
+    it('1 = 1', () => {
+      expect(1).toBe(1);
+    });
+});
